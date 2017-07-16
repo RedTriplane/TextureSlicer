a6d7651722b21074fc0e6bce10dbf47c40ef23bf
J Fixby <github@jfixby.com>
2017-07-16 20:10:10 +0200

    1.4

--- texture-slicer-io/src/com/jfixby/texture/slicer/api/io/SlicesCompositionsContainer.java
+++ texture-slicer-io/src/com/jfixby/texture/slicer/api/io/SlicesCompositionsContainer.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class SlicesCompositionsContainer implements Serializable {
 
-	public final String PACKAGE_FORMAT = "RedTriplane.TiledRaster";
+	public static final String PACKAGE_FORMAT = "RedTriplane.TiledRaster";
 
 	private static final long serialVersionUID = 6944710849959772300L;
 	public ArrayList<SlicesCompositionInfo> content = new ArrayList<SlicesCompositionInfo>();
